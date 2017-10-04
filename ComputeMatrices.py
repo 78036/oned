69fad3fe84ad1910ccb4a14e4df133451f0482cf
Kushal Hada <kushaldeveloper@gmail.com>
2017-10-04 16:47:25 -0400

    auto pep8

--- ComputeMatrices.py
+++ ComputeMatrices.py
@@ -18,7 +18,10 @@ def compute_distance_naive(X):
         for j in range(N):
             xi = X[i, :]
             xj = X[i, :]
-            dist = 0.0  # a placetaker line,
+            sum = 0.0
+            for k in range(len(xi)):
+                sum = sum + (xj[k] - xi[k]) * (xj[k] - xi[k])
+            dist = np.sqrt(sum)  # a placetaker line,
             # you have to change it to distance between xi and xj
             M[i, j] = dist
 
@@ -73,9 +76,11 @@ def main():
     params = range(10, 141, 10)  # different param setting
     nparams = len(params)  # number of different parameters
 
-    perf_dist_loop = np.zeros([10, nparams])  # 10 trials = 10 rows, each parameter is a column
+    # 10 trials = 10 rows, each parameter is a column
+    perf_dist_loop = np.zeros([10, nparams])
     perf_dist_cool = np.zeros([10, nparams])
-    perf_corr_loop = np.zeros([10, nparams])  # 10 trials = 10 rows, each parameter is a column
+    # 10 trials = 10 rows, each parameter is a column
+    perf_corr_loop = np.zeros([10, nparams])
     perf_corr_cool = np.zeros([10, nparams])
 
     counter = 0
@@ -99,8 +104,8 @@ def main():
             et = time.time()
             perf_dist_cool[i, counter] = et - st
 
-            assert np.allclose(dist_loop, dist_cool,
-                               atol=1e-06)  # check if the two computed matrices are identical all the time
+            # check if the two computed matrices are identical all the time
+            assert np.allclose(dist_loop, dist_cool, atol=1e-06)
 
             # compute correlation matrices
             st = time.time()
@@ -113,20 +118,27 @@ def main():
             et = time.time()
             perf_corr_cool[i, counter] = et - st
 
-            assert np.allclose(corr_loop, corr_cool,
-                               atol=1e-06)  # check if the two computed matrices are identical all the time
+            # check if the two computed matrices are identical all the time
+            assert np.allclose(corr_loop, corr_cool, atol=1e-06)
 
         counter = counter + 1
 
-    mean_dist_loop = np.mean(perf_dist_loop, axis=0)  # mean time for each parameter setting (over 10 trials)
+    # mean time for each parameter setting (over 10 trials)
+    mean_dist_loop = np.mean(perf_dist_loop, axis=0)
     mean_dist_cool = np.mean(perf_dist_cool, axis=0)
     std_dist_loop = np.std(perf_dist_loop, axis=0)  # standard deviation
     std_dist_cool = np.std(perf_dist_cool, axis=0)
 
     plt.figure(1)
-    plt.errorbar(params, mean_dist_loop[0:nparams], yerr=std_dist_loop[0:nparams], color='red',
+    plt.errorbar(params,
+                 mean_dist_loop[0:nparams],
+                 yerr=std_dist_loop[0:nparams],
+                 color='red',
                  label='Loop Solution for Distance Comp')
-    plt.errorbar(params, mean_dist_cool[0:nparams], yerr=std_dist_cool[0:nparams], color='blue',
+    plt.errorbar(params,
+                 mean_dist_cool[0:nparams],
+                 yerr=std_dist_cool[0:nparams],
+                 color='blue',
                  label='Matrix Solution for Distance Comp')
     plt.xlabel('Number of Cols of the Matrix')
     plt.ylabel('Running Time (Seconds)')
@@ -136,22 +148,29 @@ def main():
     # plt.show()    # uncomment this if you want to see it right way
     print("result is written to CompareDistanceCompFig.png")
 
-    mean_corr_loop = np.mean(perf_corr_loop, axis=0)  # mean time for each parameter setting (over 10 trials)
+    # mean time for each parameter setting (over 10 trials)
+    mean_corr_loop = np.mean(perf_corr_loop, axis=0)
     mean_corr_cool = np.mean(perf_corr_cool, axis=0)
     std_corr_loop = np.std(perf_corr_loop, axis=0)  # standard deviation
     std_corr_cool = np.std(perf_corr_cool, axis=0)
 
     plt.figure(2)
-    plt.errorbar(params, mean_corr_loop[0:nparams], yerr=std_corr_loop[0:nparams], color='red',
+    plt.errorbar(params,
+                 mean_corr_loop[0:nparams],
+                 yerr=std_corr_loop[0:nparams],
+                 color='red',
                  label='Loop Solution for Correlation Comp')
-    plt.errorbar(params, mean_corr_cool[0:nparams], yerr=std_corr_cool[0:nparams], color='blue',
+    plt.errorbar(params,
+                 mean_corr_cool[0:nparams],
+                 yerr=std_corr_cool[0:nparams],
+                 color='blue',
                  label='Matrix Solution for Correlation Comp')
     plt.xlabel('Number of Cols of the Matrix')
     plt.ylabel('Running Time (Seconds)')
     plt.title('Comparing Correlation Computation Methods')
     plt.legend()
     plt.savefig('CompareCorrelationCompFig.png')
-    #plt.show()  # uncomment this if you want to see it right way
+    # plt.show()  # uncomment this if you want to see it right way
     print("result is written to CompareCorrelationCompFig.png")
 
 
